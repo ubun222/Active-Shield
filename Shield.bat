7af32516081bc45a519ed5e1bbc971b48181bb08
cb <cb@iMac.local>
2023-03-06 11:55:13 +0800

    gx

--- Shield.bat
+++ Shield.bat
@@ -1,14 +1,16 @@
-timeout /T 20 /NOBREAK
+timeout /T 16 /NOBREAK
 TASKKILL /F  /IM "NVIDIA Geforce Experience.exe"
 timeout /T 2 /NOBREAK
 net stop NvContainerLocalSystem
 netsh interface ip set address "WLAN 3" static  192.168.2.115 255.255.255.0 192.168.2.97 1
 netsh interface ip set dns "WLAN 3"  static 8.8.4.4
-timeout /T 2 /NOBREAK
+timeout /T 10 /NOBREAK
 net start NvContainerLocalSystem
-timeout /T 8 /NOBREAK
-explorer C:\Users\Public\Desktop\GeForce Experience.lnk
-timeout /T 20 /NOBREAK
+timeout /T 10 /NOBREAK
+
 netsh interface ip set address "WLAN 3" DHCP
 netsh interface ip set dns "WLAN 3" DHCP
-timeout /T 3 /NOBREAK
+timeout /T 5 /NOBREAK
+
+explorer C:\Users\Public\Desktop\GeForce Experience.lnk
+pause
