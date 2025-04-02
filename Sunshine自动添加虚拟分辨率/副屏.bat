a1874099e3fb97be2dfd42169976c652fe443266
root <root@AMD>
2025-04-02 14:08:54 +0000

    gx

--- Sunshine自动添加虚拟分辨率/副屏.bat
+++ Sunshine自动添加虚拟分辨率/副屏.bat
@@ -1,11 +1,16 @@
-REM sunshine Application  Do Command : cmd /C "C:\Users\xxxxx\Documents\副屏.bat"
-REM sunshine Application  Undo Command : cmd /C "C:\Users\xxxxx\Documents\主屏.bat"
+REM sunshine Application  Do Command : cmd /C "C:\Users\cb199\Documents\副屏.bat"
+REM sunshine Application  Undo Command : cmd /C "C:\Users\cb199\Documents\主屏.bat"
 
 pnputil /disable-device  "ROOT\DISPLAY\0000"
 
-timeout /t 2 |  echo |  C:\VirtualDisplayDriver\res++.bat %SUNSHINE_CLIENT_WIDTH% %SUNSHINE_CLIENT_HEIGHT%
+timeout /t 1 |  C:\VirtualDisplayDriver\res++.bat %SUNSHINE_CLIENT_WIDTH% %SUNSHINE_CLIENT_HEIGHT%
 
 pnputil /enable-device  "ROOT\DISPLAY\0000"
 
+timeout /t 1 /NOBREAK
+
 C:\Windows\System32\DisplaySwitch.exe /external
+
+timeout /t 1 /NOBREAK
+
 C:\Windows\System32\QRes.exe /X:%SUNSHINE_CLIENT_WIDTH% /Y:%SUNSHINE_CLIENT_HEIGHT% /R:%SUNSHINE_CLIENT_FPS%
